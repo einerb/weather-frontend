d05f1f33d8968a91696065b91543c2157da466f3
Einer Bravo <einerbravo@gmail.com>
2020-06-29 04:50:47 -0500

    Major changes and removing navbar.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WeatherService } from '../../services/weather.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  public weatherData;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    this.getWeather();
   }
 
+  private getWeather() {
+    this.weatherService.getByLocation('4.6021,-74.1026').subscribe((res) => {
+      this.weatherData = res;
+
+      console.log(this.weatherData);
+      
+    });
+  }
 }
